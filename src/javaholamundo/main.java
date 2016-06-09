0e8f7713d387f2cc6229c3cecdcf11a14086060e
jportilla <jportilla@LAP-00104.navsat.local>
2016-06-09 11:57:57 -0600

    Version 09062016

--- src/javaholamundo/main.java
+++ src/javaholamundo/main.java
@@ -7,6 +7,8 @@ public class main {
 		System.out.println("hola mundo2");
 		System.out.println("hola mundo3");
 		System.out.println("hola mundo4");
+		System.out.println("hola mundo5");
+		System.out.println("hola mundo6");
 	}
 
 }
